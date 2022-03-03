2bd5e80bce90ebc9a8372426d8128d0e8f521e3c
AngelMaldonado <amaldonadojuarez@gmail.com>
2022-03-03 07:54:11 -0600

    #3 - Implementación con shaders lista (crear clase shader)

--- [3]LectorOBJ/include/containers.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Angel Maldonado on 3/3/2021.
-//
-
-#include <vector>
-#include <list>
-
-using std::vector;
-using std::list;
--- [3]LectorOBJ/src/main.cpp
+++ [3]LectorOBJ/src/main.cpp
@@ -21,13 +21,24 @@
 // Codigo para cargar shaders como programas
 #include "../include/shader.hpp"
 // Lector obj
-#include "../include/objreader.h"
+#include "../include/objloader.h"
+// Mesh
+#include "../include/mesh.h"
+
+using namespace glm;
 
 // Referencia global a la ventana
 GLFWwindow* ventana;
 
+// Matriz model-view-projection
+mat4 mvp;
+// Matriz de normales
+mat4 normal;
+
 // Funcion para inicializar la ventana con GLFW, glew y OpenGL
 int InicializaVentana();
+// Funcion para inicializar las matrices de transformacion
+void genMatrices();
 
 // Main
 int main(void)
@@ -40,49 +51,41 @@ int main(void)
     }
 
     // Lectura del objeto
-    Object obj;
-    OBJFileReader objReader;
-    obj = objReader.readFile("../obj/test/cube.obj", 'n');
+    // Toda la inicializacion del VBO y VAO se hace en el constructor de Mesh
+    Mesh mesh("../obj/test/monkey1.obj");
 
-    // Extraccion de los vertices como vectores
-    float* vertices = obj.verticesAsArray();
+    GLuint programaID = LoadShaders("C:\\Users\\amald\\OneDrive - Universidad Autonoma de San Luis Potosi - UASLP\\Programacion\\Programacion de Videojuegos\\[3]LectorOBJ\\shaders\\vs1.glsl",
+                                    "C:\\Users\\amald\\OneDrive - Universidad Autonoma de San Luis Potosi - UASLP\\Programacion\\Programacion de Videojuegos\\[3]LectorOBJ\\shaders\\fs1.glsl");
 
-    // Buffer de objetos de vertices
-    unsigned int VBO;
-    // Objeto de arreglo de indices de vertices
-    unsigned int VAO;
-
-    // Crear el buffer de memoria para los vertices
-    glGenBuffers(1, &VBO);
-    // Crear el buffer de memoria para los indices
-    glGenVertexArrays(1, &VAO);
-
-    // Asignar el buffer de memoria para los indices
-    glBindVertexArray(VAO);
-    // Asignar el buffer de memoria para los vertices
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // Asignar los datos del buffer de memoria para los vertices
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // Asignar los datos del buffer de memoria para los indices
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    // Habilitar el buffer de memoria para los vertices
-    glEnableVertexAttribArray(0);
-
-    // Se crean 2 programas de shaders (vertex y fragment)
-    GLuint programaID = LoadShaders("C:\\Users\\amald\\OneDrive - Universidad Autonoma de San Luis Potosi - UASLP\\Programacion\\Programacion de Videojuegos\\[2]Triangulo\\vs1.glsl",
-                                    "C:\\Users\\amald\\OneDrive - Universidad Autonoma de San Luis Potosi - UASLP\\Programacion\\Programacion de Videojuegos\\[2]Triangulo\\fs1.glsl");
-
-    // Seleccionar el programa de shaders
-    glUseProgram(programaID);
-    glBindVertexArray(VAO);
-
+    
     // Ciclo principal
     do
     {
         // Limpia la ventana de color
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        // Dibujar el triangulo
-        glDrawArrays(GL_QUADS, 0, obj.countTotalVertices());// Primer indice, cantidad de vertices
+
+        // Seleccionar el programa de shaders
+        glUseProgram(programaID);
+
+        // Variables uniformes
+        // Matrices de transformacion
+        genMatrices();
+        // // Guarda las matrices en el buffer-shader
+        int mvpLoc = glGetUniformLocation(programaID, "MVP");
+        glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, value_ptr(mvp));
+        // Normal
+        int normalLoc = glGetUniformLocation(programaID, "Normal");
+        glUniformMatrix4fv(normalLoc, 1, GL_FALSE, value_ptr(normal));
+        // Color del modelo
+        int colorLoc = glGetUniformLocation(programaID, "Color");
+        glUniform3f(colorLoc, 0.0f, 0.0f, 1.0f);
+        // Direccion de la luz
+        // int lightDirLoc = glGetUniformLocation(programaID, "LightDir");
+        // glUniform3f(lightDirLoc, 0.0f, 0.0f, 1.0f);
+
+        // Dibuja el objeto
+        // Las operaciones Bind y dibujado del objeto se hacen en esta funcion
+        mesh.Draw();
 
         // Actualizar la ventana (buffer)
         glfwSwapBuffers(ventana);
@@ -92,8 +95,6 @@ int main(void)
     }while(glfwGetKey(ventana, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(ventana) == 0);
 
     // Liberar los recursos
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
     glDeleteProgram(programaID);
     // Cerrar la ventana
     glfwTerminate();
@@ -147,4 +148,23 @@ int InicializaVentana()
 
     // La ventana se creo correctamente
     return 1;
+}
+
+void genMatrices() {
+    // Model-view projection
+    mat4 model = mat4(1); // model
+    model = translate(model, vec3(0.0f, 0.0f, -5.0f));
+    mat4 view = mat4(1); // view
+    vec3 eye = vec3(0.0f, 1.0f, 5.0f);
+    vec3 center = vec3(0.0f, 0.0f, 0.0f);
+    vec3 up = vec3(0.0f, 1.0f, 0.0f);
+    view = lookAt(eye, center, up);
+    mat4 projection = perspective(radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f); // projection
+    
+    mvp = projection * view * model;
+
+    // Normal
+    normal = model;
+
+    // 
 }
