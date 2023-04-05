d8c67d698009687729816d0ee3fb40d5f5072ba9
AngelMaldonado <amaldonadojuarez@gmail.com>
2023-04-05 17:17:08 -0600

    Shader.h y stb_image

--- Curso2(Jully)/OpenGL/OpenGL/main.cpp
+++ Curso2(Jully)/OpenGL/OpenGL/main.cpp
@@ -1,7 +1,13 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
 #include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <Shader.h>
+#include <stb_image.h>
 
 using namespace std;
 
